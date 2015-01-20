4b87a8c56cfc398d40b0212308851429f82faa2f
farcek <Farcek@gmail.com>
2015-01-20 15:00:36 +0800

    init

---
+++ src/main/java/mn/le/farcek/jbw/bundle/ui/JBWBundleUI.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mn.le.farcek.jbw.bundle.ui;
+
+
+import java.util.List;
+import mn.le.farcek.jbw.api.Bundle;
+import mn.le.farcek.jbw.api.bundle.JBWBundle;
+
+/**
+ *
+ * @author Farcek
+ */
+@Bundle(name = "ui")
+public class JBWBundleUI extends JBWBundle {
+
+    @Override
+    protected void setupControllers(List<Class<?>> controllers) {
+    }
+
+}
